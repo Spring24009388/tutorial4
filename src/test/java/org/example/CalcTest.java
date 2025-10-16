c570a1700cf4e5a10865d3c4a7bd8fd5f910df9c
Spring24009388 <3191375717@qq.com>
2025-10-16 12:55:59 +0800

    Add testSubtraction (expected to fail)

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -10,5 +10,12 @@ public class CalcTest {
     void testAddition() {
         assertEquals(4, c.add(2,2));
     }
-
+    @Test
+    public void testSubtraction() {
+        Calc c = new Calc();
+        assertEquals(2, c.subtract(4, 2));
+    }
+    public int subtract(int a, int b) {
+        return a + b;
+    }
 }
